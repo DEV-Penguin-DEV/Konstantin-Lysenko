124eebfafe82f034246a3b42282e65393dde7404
PENGUIN <kosvadmail@atomlc.com.ua>
2023-12-27 05:35:13 +0100

    Добавляет аналитику от vercel

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <template>
   <SpeedInsights />
-  <Analytics />
   <MainHeader :DB="DB.menu.content" />
   <main class="main-content">
     <FirstScreen :DB="DB.first_screen.content" />
@@ -22,7 +21,6 @@ import PortfolioSection from "@/sections/PortfolioSection";
 import LanguageSection from "@/sections/LanguageSection";
 import AppLoader from "@/sections/AppLoader";
 import { SpeedInsights } from "@vercel/speed-insights/vue";
-import { Analytics } from "@vercel/analytics/react";
 import { db } from "@/db/db.js";
 
 export default {
@@ -35,7 +33,6 @@ export default {
     LanguageSection,
     AppLoader,
     SpeedInsights,
-    Analytics,
   },
   data() {
     return {
